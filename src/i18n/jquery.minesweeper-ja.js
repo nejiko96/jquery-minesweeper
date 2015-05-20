3694b06cec3effb267ec83b55d1d665ded28e063
nejiko96 <nejiko2006@gmail.com>
2015-05-21 00:00:43 +0900

    clean : convert tabs to spaces

--- src/i18n/jquery.minesweeper-ja.js
+++ src/i18n/jquery.minesweeper-ja.js
@@ -1,11 +1,11 @@
 (function($) {
-	$.minesweeper.regional['ja'] = {
-		minesTextBefore: 'あと',
-		minesTextAfter: '個',
-		timerTextBefore: '',
-		timerTextAfter: '秒経過',
-		restartText: 'もう一回？',
-		clearedText: 'クリア！'
-	};
-	$.extend($.minesweeper.defaults, $.minesweeper.regional['ja']);
+  $.minesweeper.regional['ja'] = {
+    minesTextBefore: 'あと',
+    minesTextAfter: '個',
+    timerTextBefore: '',
+    timerTextAfter: '秒経過',
+    restartText: 'もう一回？',
+    clearedText: 'クリア！'
+  };
+  $.extend($.minesweeper.defaults, $.minesweeper.regional['ja']);
 })(jQuery);
