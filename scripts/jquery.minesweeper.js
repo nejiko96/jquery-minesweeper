3d7e63019c5497b4cf8e081d4a97436ea808371e
nejiko96 <nejiko2006@gmail.com>
2015-05-24 06:18:27 +0900

    Release v1.4.2

--- scripts/jquery.minesweeper.js
+++ scripts/jquery.minesweeper.js
@@ -1,6 +1,6 @@
 /**
  * jQuery minesweeper plugin
- * Version: 1.4.1
+ * Version: 1.4.2
  * Date: 2015/05/23
  * @requires jQuery v1.4.4 or later
  * @requires jQuery.timers (optional)
@@ -1068,7 +1068,7 @@
 
   // バージョン情報・初期設定
   $.minesweeper = {};
-  $.minesweeper.version = "1.4.1";
+  $.minesweeper.version = "1.4.2";
   $.minesweeper.defaults =
   {
     level: 'easy'
